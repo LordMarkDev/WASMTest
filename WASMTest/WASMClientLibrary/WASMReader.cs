eacc6c49417ec60da60f716cc6ef88e396aed645
Marco Negroni <lordmarkdev@gmail.com>
2022-08-08 19:06:34 +0200

    -WASMTest solution

---
+++ WASMTest/WASMClientLibrary/WASMReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+using WASMClientLibrary.Properties;
+using WebAssembly;
+using WebAssembly.Runtime;
+
+namespace WASMClientLibrary
+{
+    public class WASMReader
+    {
+        public int ExecuteWasSum(int a, int b)
+        {
+            using (MemoryStream memoryStream = new MemoryStream((byte[])new ResourceManager(typeof(Resources)).GetObject("TestWasm")))
+            {
+                ImportDictionary importDictionary = new ImportDictionary();
+
+                Instance<dynamic> webAssemblyInstance = Compile.FromBinary<dynamic>(memoryStream)(importDictionary);
+
+                return (int)webAssemblyInstance.Exports.add(a, b);
+            }
+        }
+    }
+}
---
+++ WASMTest/WASMTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WASMClientLibrary;
+
+namespace WASMTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WASMReader wasmReader = new WASMReader();
+
+            Console.WriteLine(wasmReader.ExecuteWasSum(1, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
